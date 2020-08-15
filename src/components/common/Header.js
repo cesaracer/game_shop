8ababa8c9e43e3d312beed5bc8ed7fa220e4fc87
cesar r <cesaracer@gmail.com>
2020-08-15 13:21:07 -0600

    added classnames to components

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,13 +8,13 @@ const Header = (props) => {
             <div className="navbar">
                 <div id="logo" style={{position: "relative", float: "left"}}><img style={{height: "100px"}} src="images/logo.svg"/></div>
                 <div id="items" style={{paddingTop: "20px"}}>
-                    <div class="navbar-item"><Link style={{color: "dimgray", textDecoration: "none", fontSize: "30px"}} to="">Home</Link></div>
-                    <div class="navbar-item"> | </div>
-                    <div class="navbar-item"><Link style={{color: "dimgray", textDecoration: "none", fontSize: "30px"}} to="/shop">Shop</Link></div>
-                    <div class="navbar-item"> | </div>
-                    <div class="navbar-item"><Link style={{color: "dimgray", textDecoration: "none", fontSize: "30px"}} to="/contact">Contact</Link></div>  
+                    <div><Link className="navbar-item" to="">Home</Link></div>
+                    <div className="navbar-item"> | </div>
+                    <div><Link className="navbar-item" to="/shop">Shop</Link></div>
+                    <div className="navbar-item">| </div>
+                    <div ><Link className="navbar-item" to="/contact">Contact</Link></div>  
                 </div>
-                <div class="cart-icon">
+                <div className="cart-icon">
                     <Link to="/cart"><img src="images/cart.png" id="cart-icon-img" alt="shopping cart icon"/>
                         <p>{props.count}</p>
                     </Link>
